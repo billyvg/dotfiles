98c88d4953e5a3d592f24209d6c619ba4fc137e4
Billy Vong <billy@sentry.io>
2021-07-12 09:42:13 -0700

    add packer-bootstrap

---
+++ .config/nvim/lua/packer-bootstrap.lua
@@ -0,0 +1,7 @@
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+print(install_path)
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.api.nvim_command('packadd packer.nvim')
+end
