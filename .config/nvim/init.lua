5c8d5b8298ae7471c803da539fbccca599ba32ac
Billy Vong <billyvg@users.noreply.github.com>
2022-07-17 10:51:48 -0400

    nvim: Migrate from `compe` to `cmp`

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -146,94 +146,99 @@ require("packer").startup({
 
 		-- {{{ Plugins: LSP
 
-		-- {{{ Plugin: Autocompletion
-		use({
-			"hrsh7th/nvim-compe",
-			config = function()
-				vim.o.completeopt = "menuone,noselect"
-				require("compe").setup({
-					enabled = true,
-					autocomplete = true,
-					debug = false,
-					min_length = 1,
-					preselect = "enable",
-					throttle_time = 80,
-					source_timeout = 200,
-					resolve_timeout = 800,
-					incomplete_delay = 400,
-					max_abbr_width = 100,
-					max_kind_width = 100,
-					max_menu_width = 100,
-					documentation = {
-						border = { "", "", "", " ", "", "", "", " " }, -- the border option is the same as `|help nvim_open_win|`
-						winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-						max_width = 120,
-						min_width = 60,
-						max_height = math.floor(vim.o.lines * 0.3),
-						min_height = 1,
-					},
+		-- {{{ Plugin: Snippets
+		use("L3MON4D3/LuaSnip")
+		-- }}}
 
-					source = {
-						path = true,
-						buffer = true,
-						calc = true,
-						nvim_lsp = true,
-						nvim_lua = true,
-						vsnip = false,
-						ultisnips = false,
-						luasnip = false,
+		-- {{{ Plugin: Autocompletion
+		use("hrsh7th/cmp-nvim-lsp")
+		use("hrsh7th/cmp-buffer")
+		use("hrsh7th/cmp-path")
+		use("hrsh7th/cmp-cmdline")
+		-- use("f3fora/cmp-spell")
+		use("hrsh7th/cmp-calc")
+		use("hrsh7th/cmp-emoji")
+
+		use({
+			"hrsh7th/nvim-cmp",
+			requires = {
+				{ "L3MON4D3/LuaSnip" },
+			},
+			config = function()
+				local cmp = require("cmp")
+				local luasnip = require("luasnip")
+				local lspkind = require("lspkind")
+
+				local has_words_before = function()
+					local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+					return col ~= 0
+						and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+				end
+
+				cmp.setup({
+					experimental = { ghost_text = false },
+					snippet = {
+						-- REQUIRED - you must specify a snippet engine
+						expand = function(args)
+							require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+						end,
+					},
+					mapping = cmp.mapping.preset.insert({
+						["<C-b>"] = cmp.mapping.scroll_docs(-4),
+						["<C-f>"] = cmp.mapping.scroll_docs(4),
+						["<C-Space>"] = cmp.mapping.complete(),
+						["<C-e>"] = cmp.mapping.abort(),
+						["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+						["<Tab>"] = cmp.mapping(function(fallback)
+							if cmp.visible() then
+								cmp.select_next_item()
+							elseif luasnip.expand_or_jumpable() then
+								luasnip.expand_or_jump()
+							elseif has_words_before() then
+								cmp.complete()
+							else
+								fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+							end
+						end, { "i", "s" }),
+						["<S-Tab>"] = cmp.mapping(function()
+							if cmp.visible() then
+								cmp.select_prev_item()
+							elseif luasnip.jumpable(-1) then
+								luasnip.jump(-1)
+							end
+						end, { "i", "s" }),
+					}),
+					sources = cmp.config.sources({
+						{ name = "nvim_lsp" },
+						{ name = "buffer", keyword_length = 5 },
+						{ name = "luasnip" },
+						{ name = "calc" },
+						{ name = "emoji" },
+						-- { name = "spell" },
+					}),
+
+					formatting = {
+						format = lspkind.cmp_format({
+							mode = "symbol", -- show only symbol annotations
+							maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+							-- The function below will be called before any actual modifications from lspkind
+							-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+							-- before = function (entry, vim_item)
+							--   ...
+							--   return vim_item
+							-- end
+						}),
 					},
 				})
-				local t = function(str)
-					return vim.api.nvim_replace_termcodes(str, true, true, true)
-				end
 
-				local check_back_space = function()
-					local col = vim.fn.col(".") - 1
-					return col == 0 or vim.fn.getline("."):sub(col, col):match("%s") ~= nil
-				end
+				-- Use buffer source for `/`.
+				cmp.setup.cmdline("/", { sources = { { name = "buffer" } } })
 
-				-- Use (s-)tab to:
-				--- move to prev/next item in completion menuone
-				--- jump to prev/next snippet's placeholder
-				_G.tab_complete = function()
-					if vim.fn.pumvisible() == 1 then
-						return t("<C-n>")
-					-- elseif vim.fn['vsnip#available'](1) == 1 then
-					-- return t "<Plug>(vsnip-expand-or-jump)"
-					elseif check_back_space() then
-						return t("<Tab>")
-					else
-						return vim.fn["compe#complete"]()
-					end
-				end
-
-				_G.s_tab_complete = function()
-					if vim.fn.pumvisible() == 1 then
-						return t("<C-p>")
-					-- elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-					-- return t "<Plug>(vsnip-jump-prev)"
-					else
-						-- If <S-Tab> is not working in your terminal, change it to <C-h>
-						return t("<S-Tab>")
-					end
-				end
-
-				vim.keymap.set("i", "<Tab>", "v:lua.tab_complete()", { expr = true })
-				vim.keymap.set("s", "<Tab>", "v:lua.tab_complete()", { expr = true })
-				vim.keymap.set("i", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
-				vim.keymap.set("s", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
-
-				vim.keymap.set("i", "<C-Space>", "compe#complete()", { expr = true, silent = true })
-				vim.keymap.set(
-					"i",
-					"<CR>",
-					[[compe#confirm(luaeval("require 'nvim-autopairs'.autopairs_cr()"))]],
-					{ expr = true, silent = true }
-				)
-				vim.keymap.set("i", "<C-e>", "compe#close('<C-e>')", { expr = true, silent = true })
-				vim.keymap.set("i", "<C-f>", "compe#scroll({ 'delta': +4 })", { expr = true, silent = true })
-				vim.keymap.set("i", "<C-d>", "compe#scroll({ 'delta': -4 })", { expr = true, silent = true })
+				-- Use cmdline & path source for ':'.
+				cmp.setup.cmdline(":", {
+					sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+				})
 			end,
 		})
 		-- }}}
@@ -380,9 +385,7 @@ require("packer").startup({
 		-- This tiny plugin adds vscode-like pictograms to neovim built-in LSP
 		use({
 			"onsails/lspkind-nvim",
-			config = function()
-				require("lspkind").init()
-			end,
+			-- setup in cmp plugin
 		})
 
 		-- This plugin makes the Neovim LSP client use FZF to display results and navigate the code.
@@ -576,10 +579,10 @@ require("packer").startup({
 			config = function()
 				local npairs = require("nvim-autopairs")
 				npairs.setup()
-				require("nvim-autopairs.completion.compe").setup({
-					map_cr = true, --  map <CR> on insert mode
-					map_complete = true, -- it will auto insert `(` after select function or method item
-				})
+
+				local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+				local cmp = require("cmp")
+				cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 				-- -- skip it, if you use another global object
 				-- _G.MUtils= {}
