bd5c8797348275945a847a7ee8ffd8ae69718ed6
Billy Vong <billyvg@users.noreply.github.com>
2022-07-22 09:24:45 -0400

    nvim: update Trouble hotkeys

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -342,11 +342,11 @@ require("packer").startup({
 			requires = "kyazdani42/nvim-web-devicons",
 			cmd = { "Trouble" },
 			setup = function()
-				vim.keymap.set("n", "<leader>xx", "<cmd>Trouble<cr>", { silent = true })
+				vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true })
 				-- vim.keymap.set("n", "<leader>xw", "<cmd>Trouble workspace_diagnostics<cr>", { silent = true })
 				-- vim.keymap.set("n", "<leader>xd", "<cmd>Trouble document_diagnostics<cr>", { silent = true })
 				-- vim.keymap.set("n", "<leader>xl", "<cmd>Trouble locist<cr>", { silent = true })
-				-- vim.keymap.set("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", { silent = true })
+				vim.keymap.set("n", "<leader>xq", "<cmd>Trouble quickfix<cr>", { silent = true })
 				-- vim.keymap.set("n", "<leader>gR", "<cmd>Trouble lsp_references<cr>", { silent = true })
 			end,
 			config = function()
