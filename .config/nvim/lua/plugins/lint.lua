a34b21a70e32f507b402b8d7f30dae8c414d3e2c
Billy Vong <billyvg@users.noreply.github.com>
2025-02-11 13:15:30 -0500

    nvim(lint): run linters only if config file is present

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -1,41 +1,64 @@
 local js_linters = { "biomejs", "eslint_d" }
+-- only run linters if a configuration file is found for the below linters
+local linter_root_markers = {
+  biomejs = { "biome.json", "biome.jsonc" },
+  eslint_d = {
+    "eslint.config.js",
+    "eslint.config.mjs",
+    "eslint.config.cjs",
+    "eslint.config.ts",
+    "eslint.config.mts",
+    "eslint.config.cts",
+    -- deprecated
+    ".eslintrc.js",
+    ".eslintrc.cjs",
+    ".eslintrc.yaml",
+    ".eslintrc.yml",
+    ".eslintrc.json",
+  },
+}
 
 return {
-	{
-		"mfussenegger/nvim-lint",
-		opts = {
-			linters_by_ft = {
-				typescript = js_linters,
-				javascript = js_linters,
-				typescriptreact = js_linters,
-				javascriptreact = js_linters,
-			},
-		},
-		config = function()
-			local lint = require("lint")
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        typescript = js_linters,
+        javascript = js_linters,
+        typescriptreact = js_linters,
+        javascriptreact = js_linters,
+      },
+    },
+    config = function(_, opts)
+      -- Create autocommand which carries out the actual linting
+      -- on the specified events.
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          local lint = require("lint")
 
-			-- Allow other plugins to add linters to require('lint').linters_by_ft
-			lint.linters_by_ft = lint.linters_by_ft or {}
+          -- Only run the linter in buffers that you can modify in order to
+          -- avoid superfluous noise, notably within the handy LSP pop-ups that
+          -- describe the hovered symbol using Markdown.
+          if vim.opt_local.modifiable:get() then
+            local names = opts.linters_by_ft[vim.bo.filetype]
 
-			lint.linters_by_ft["typescript"] = js_linters
-			lint.linters_by_ft["javascript"] = js_linters
-			lint.linters_by_ft["typescriptreact"] = js_linters
-			lint.linters_by_ft["javascriptreact"] = js_linters
+            if names then
+              for _, name in pairs(names) do
+                local next = next
 
-			-- Create autocommand which carries out the actual linting
-			-- on the specified events.
-			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-				group = lint_augroup,
-				callback = function()
-					-- Only run the linter in buffers that you can modify in order to
-					-- avoid superfluous noise, notably within the handy LSP pop-ups that
-					-- describe the hovered symbol using Markdown.
-					if vim.opt_local.modifiable:get() then
-						lint.try_lint()
-					end
-				end,
-			})
-		end,
-	},
+                -- use vim.fs.find to look for a config file in *any* parent directories
+                if
+                  linter_root_markers[name] == nil or next(vim.fs.find(linter_root_markers[name], { upward = true }))
+                then
+                  lint.try_lint(name)
+                end
+              end
+            end
+          end
+        end,
+      })
+    end,
+  },
 }
